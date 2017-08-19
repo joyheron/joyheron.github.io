b1f69b1e59f57d2349832d413547d8ec17a7584f
Joy Clark <joy.clark@innoq.com>
2017-08-19 22:59:24 +0200

    add thumbnails

--- _scripts/mod_img.rb
+++ _scripts/mod_img.rb
@@ -1,22 +1,27 @@
 require 'find'
 require 'fileutils'
 
+def convert_image(original, out, image_opts)
+	new_name = original.sub '_raw_img', out
+	new_dir = File.dirname(new_name)
+	unless File.directory?(new_dir)
+		puts "creating #{new_dir}"
+		FileUtils.mkdir_p(new_dir)
+	end
+
+	if File.file?(new_name) 
+		puts "Skipping #{original}, #{new_name} already exists"
+	else 
+		cmd = "convert #{original} #{image_opts} #{new_name}"
+		puts "converting with #{image_opts} #{original}-->#{new_name}"
+		system cmd
+	end
+end
+
 def convert(file)
 	if (!File.directory?(file) && file[".jpg"])
-		new_name = file.sub '_raw_img', 'img'
-		new_dir = File.dirname(new_name)
-		unless File.directory?(new_dir)
-			puts "creating #{new_dir}"
-			FileUtils.mkdir_p(new_dir)
-		end
-
-		if File.file?(new_name) 
-			puts "Skipping #{file}, #{new_name} already exists"
-		else 
-			cmd = "convert #{file} -resize 750 -strip -quality 86 #{new_name}"
-			puts "converting... #{file}-->#{new_name}"
-			system cmd
-		end
+		convert_image(file, "img", "-resize 750 -strip -quality 86")
+		convert_image(file, "img/thumbnail", "-resize 200 -strip -quality 86")
 	end
 end
 
